d214bea819ad73c3996cbd1bdd002b23f2af255d
Mahmoud Emam <mahmoud.emam@ashtrywla.com>
2018-07-21 19:35:13 +0200

    adds user settings for default brand and limit to load mobiles

--- app/src/main/java/com/udacity/nd/projects/mobfinder/MainActivity.java
+++ app/src/main/java/com/udacity/nd/projects/mobfinder/MainActivity.java
@@ -1,11 +1,16 @@
 package com.udacity.nd.projects.mobfinder;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,6 +19,7 @@ import android.widget.Spinner;
 
 import com.udacity.nd.projects.mobfinder.adapters.MobileAdapter;
 import com.udacity.nd.projects.mobfinder.data.Mobile;
+import com.udacity.nd.projects.mobfinder.settings.SettingsActivity;
 import com.udacity.nd.projects.mobfinder.utils.NetworkUtils;
 
 import java.io.IOException;
@@ -23,7 +29,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements Callback<List<Mobile>> {
+public class MainActivity extends AppCompatActivity implements Callback<List<Mobile>>, SharedPreferences.OnSharedPreferenceChangeListener {
     private static final String TAG = MainActivity.class.getSimpleName();
     private RecyclerView rv;
     private GridLayoutManager gridLayoutManager;
@@ -35,13 +41,48 @@ public class MainActivity extends AppCompatActivity implements Callback<List<Mob
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setupSpinner();
         progressBar = findViewById(R.id.pb_loading_mobiles);
+
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(this);
+
+        setupSpinner();
 
         rv = findViewById(R.id.rv_mobiles);
         gridLayoutManager = new GridLayoutManager(this, getResources().getInteger(R.integer.grid_count));
 
-        loadMobiles("Samsung");
+        loadMobiles();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = new MenuInflater(this);
+
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        Log.d(TAG, "menuItem=" + item.getTitle().toString());
+
+        switch (itemId) {
+            case R.id.menu_settings:
+                SettingsActivity.launch(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void setupSpinner() {
@@ -57,7 +98,7 @@ public class MainActivity extends AppCompatActivity implements Callback<List<Mob
                 String brand = adapterView.getAdapter().getItem(pos).toString();
                 Log.d(TAG, brand);
 
-                loadMobiles(brand);
+                loadMobiles();
             }
 
             @Override
@@ -65,13 +106,38 @@ public class MainActivity extends AppCompatActivity implements Callback<List<Mob
 
             }
         });
+
+        updateSpinnerSelectionFromPreference();
     }
 
-    private void loadMobiles(String brand) {
+    private void updateSpinnerSelectionFromPreference() {
+        int brand = getDefaultBrand();
+        if (brand != -1)
+            spinner.setSelection(brand);
+    }
+
+    private int getDefaultBrand() {
+        return Integer.valueOf(PreferenceManager.getDefaultSharedPreferences(this)
+                .getString(getString(R.string.pref_key_brand), "-1"));
+    }
+
+    private void loadMobiles() {
         progressBar.setVisibility(View.VISIBLE);
         rv.setVisibility(View.INVISIBLE);
 
-        NetworkUtils.getLatest(this, brand, 10);
+        int limit = Integer.valueOf(PreferenceManager.getDefaultSharedPreferences(this)
+                .getString(getString(R.string.pref_key_no_mobiles), "10"));
+
+        NetworkUtils.getLatest(this, spinner.getSelectedItem().toString(), limit);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.pref_key_brand))) {
+            updateSpinnerSelectionFromPreference();
+        } else if (key.equals(getString(R.string.pref_key_no_mobiles))) {
+            loadMobiles();
+        }
     }
 
     @Override
